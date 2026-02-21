9f8ad1c8a51e0385a27c97eca4ec28d6f3014886
Robert Svihla <xsvihla@fi.muni.cz>
2026-02-21 19:37:47 +0100

    add collapsable elements and tweak styling

--- app/assets/javascript/databasium/controllers/model_controller.js
+++ app/assets/javascript/databasium/controllers/model_controller.js
@@ -13,7 +13,8 @@ export default class extends Controller {
 
     const capitalizedTarget = target[0].toUpperCase() + target.slice(1)
     const capitalizedContainer = container[0].toUpperCase() + container.slice(1)
-    const closestGroup = event.currentTarget.closest(".group") 
+
+    const closestGroup = event.currentTarget.closest(`.${container}`) || event.currentTarget.closest(".group") 
     if (this[`has${capitalizedTarget}Target`] && (closestGroup || this[`has${capitalizedContainer}Target`])) {
       const content = this[`${target}Target`].innerHTML
       if (closestGroup) {
@@ -25,17 +26,5 @@ export default class extends Controller {
       console.log("You are missing a " + capitalizedTarget + " target and/or " + capitalizedContainer +  " container target please define it in the js controller and then in html template")
     }
 
-    // 1. Grab the HTML inside the <template>
-    // const target = event.params["target"] + "Target"
-    // console.log(this.hasValidationTarget)
-    // console.log(this["has" + "ValidationTarget"])
-    // if (this["has" + target.toUpperCase()]) {
-    //   console.log("Adb")
-    //   console.log(this[target]().innerHTML)
-    // }
-    // console.log(event.params) 
-    // console.log(event.params["target"]) 
-    
-    // 2. Insert it into the container
   }
 }
