6fe9cba13c0014670c36b30dfb1d64b719818319
Robert Svihla <xsvihla@fi.muni.cz>
2026-02-23 22:02:24 +0100

    update to use select for validation types

--- app/assets/javascript/databasium/controllers/attribute_controller.js
+++ app/assets/javascript/databasium/controllers/attribute_controller.js
@@ -2,14 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="attribute"
 export default class extends Controller {
-  static targets = ["name", "nameValidationInput"]
+  static targets = ["name", "nameInput", "nameValidationInput"]
 
   connect() {
   }
 
-  updateName(e){
-    const name = e.target.value
+  updateName(){
+    const name = this.nameInputTarget.value
     this.nameTarget.textContent = name
+    this.nameValidationInputTargets.forEach(target => {
+      target.value = name
+    })
+  }
+
+  updateValidationName(e){
+    // this could probably be done with single target in model controller
+    const name = this.nameInputTarget.value
     this.nameValidationInputTargets.forEach(target => {
       target.value = name
     })
