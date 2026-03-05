05ee20a8fd5fcd331147ee45cf86f7342a008cd1
Robert Svihla <xsvihla@fi.muni.cz>
2026-03-05 21:37:53 +0100

    small quality fixes

--- app/controllers/databasium/schemas_controller.rb
+++ app/controllers/databasium/schemas_controller.rb
@@ -1,14 +1,7 @@
 class Databasium::SchemasController < Databasium::ApplicationController
   def index
-    @tables = set_tables
     schema_service = Databasium::Schema.new
+    @tables = schema_service.tables
     @schema = schema_service.schema
-  end
-
-  private
-
-  def set_tables
-    @tables =
-      (ActiveRecord::Base.connection.data_sources - %w[ar_internal_metadata schema_migrations])
   end
 end
